ab5091f41c52834e204210e8d9bbab5346665a1e
taichi-de <61309248+taichi-de@users.noreply.github.com>
2021-04-04 10:25:03 +0200

    fix styles and add preloading

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
-$(function() {
-  $('#openImp').click(function() {
-    $('#impressum').fadeIn(1000);
+$(function () {
+  $("#openImp").click(function () {
+    $("#impressum").fadeIn(1000);
   });
-  $('#closeImp').click(function() {
-    $('#impressum').fadeOut(1000);
+  $("#closeImp").click(function () {
+    $("#impressum").fadeOut(1000);
   });
 });
+
+window.onload = function () {
+  let spinner = document.getElementById("preload");
+  spinner.classList.add("loaded");
+};
